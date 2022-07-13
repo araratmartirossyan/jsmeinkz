825c63fcf389433af4f1baf196b102d1bcf78af0
Sheenizle <sheenizle246@gmail.com>
2022-07-13 19:53:49 +0600

    feat(jsmein)create adoptive layout

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -6,11 +6,11 @@ import { FooterSocialMedia } from './FooterSocialMedia'
 export const Footer = () => {
   return (
     <footer className="w-full">
-      <div className="mt-20 pt-10 pb-10 border-t">
+      <div className="mt-20 sm:pt-10 pt-5 sm:pb-10 pb-5 border-t">
         <div className="max-w-3xl mx-auto">
           <FooterNavLink />
           <FooterSocialMedia />
-          <p className="flex flex-row justify-center mx-auto my-4 space-x-4 text-xl text-gray-200  ">
+          <p className="flex flex-row justify-center mx-auto my-4 space-x-4 sm:text-xl text-gray-200 text-xs ">
             © 2019-2022 JobsInJS.com. All Rights Reserved.
           </p>
         </div>
